c658ca50e42fe5570de5dacbe236a51783990b96
Chansik Yun <chansigi@gmail.com>
2021-04-21 04:41:32 +0000

    .

--- 2_테스트픽스쳐2.cpp
+++ 2_테스트픽스쳐2.cpp
@@ -1,7 +1,7 @@
 // 2_테스트픽스쳐.cpp
 class Calculator {
 public:
-	// Calculator(int n) {}
+	Calculator(int n) {}
 
 	double Display() { return 0; }
 	
@@ -22,13 +22,41 @@ public:
 
 //  테스트케이스를 만드는 매크로
 //   1) TEST: 암묵적인 테스트 스위트를 사용합니다.
+// TEST(SampleTest, foo) {} // class SampleTest_foo_Test : public testing::Test {};
+// TEST(SampleTest, goo) {} // class SampleTest_goo_Test : public testing::Test {};
+
+//                 testing::Test
+//                       |
+//                -------------------
+//                |                 |
+//            SampleTest_foo  SampleTest_goo
+
+
+//   2) TEST_F: 명시적인 테스트 스위트를 이용합니다.
+//   class SampleTest : public testing::Test {}
+//   TEST_F(SampleTest, foo) {} // class SampleTest_foo_Test : public SampleTest {};
+//   TEST_F(SampleTest, goo) {} // class SampleTest_goo_Test : public SampleTest {};
+
+//                testing::Test
+//                      |
+//                 SampleTest   
+//                      |
+//                -------------------
+//                |                 |
+//            SampleTest_foo  SampleTest_goo
 
 
 // 적용 방법.
 //  1. 테스트 스위트 클래스를 만듭니다.
 class CalculatorTest : public testing::Test {
-public:
-	Calculator* Create() { return new Caluclator(); }
+// public:
+// private:
+protected:
+	// 주의사항: 테스트케이스 안에서 테스트스위트 클래스에 정의된 함수에 접근하기 위해서는 
+	//           protected 이상 이어야 합니다.
+	// Calculator* Create() { return new Calculator; }
+	Calculator* Create() { return new Calculator(0); }
+	// 테스트 유틸리티 메소드 - Creation 메소드
 };
 
 
