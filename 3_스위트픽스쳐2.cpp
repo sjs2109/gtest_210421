e4efc690a8ac164913807c966ca501cbe45ad12e
Chansik Yun <chansigi@gmail.com>
2021-04-21 05:49:06 +0000

    .

---
+++ 3_스위트픽스쳐2.cpp
@@ -0,0 +1,91 @@
+// 3_스위트픽스쳐2.cpp
+#include <string>
+#include <unistd.h> // sleep
+
+class Database {
+public:
+	// 가정: Connect() / Disconnect()가 느리다.
+	void Connect() { sleep(2); }
+	void Disconnect() { sleep(2); }
+
+	void Login(const std::string& id, const std::string& password) {}
+	void Logout() {}
+
+	bool IsLogin() { return true; }
+};
+
+//--------------------
+#include <gtest/gtest.h>
+
+// 문제점: 픽스쳐를 설치하거나 해체하는 과정으로 인해 "느린 테스트"의 문제가 발생하였다.
+//  해결방법 => 스위트 픽스쳐(Suite Fixture)
+
+#if 0
+//----------
+<Suite Fixture SetUp> - static
+DatabaseTest* ts = new DatabaseTest;
+ts->SetUp();
+ts->Login();
+ts->TearDown();
+delete ts;
+
+DatabaseTest* ts = new DatabaseTest;
+ts->SetUp();
+ts->Logout();
+ts->TearDown();
+delete ts;
+<Suite Fixture TearDown> - static
+//----------
+#endif
+
+// 25TC * 4 = 100 sec
+//  - Slow Test(느린 테스트): 테스트 냄새
+//  : 테스트가 느려서 테스트를 수행하는 개발자의 생산성을 떨어뜨린다.
+//    테스트가 느려서 코드가 변경되어도 테스트를 수행하지 않는다.
+
+class DatabaseTest : public testing::Test {
+protected:
+	// 1.10 이전
+#if 0
+	static void SetUpTestCase() {
+		printf("SetUpTestCase\n");
+	}
+	static void TearDownTestCase() {
+		printf("TearDownTestCase\n");
+	}
+#endif
+	// 1.10 이후
+	static void SetUpTestSuite() {
+		printf("SetUpTestSuite\n");
+	}
+	static void TearDownTestSuite() {
+		printf("TearDownTestSuite\n");
+	}
+
+	Database* database;
+	DatabaseTest() : database(nullptr) {}
+
+	void SetUp() override {
+		database = new Database;
+		database->Connect();
+	}
+
+	void TearDown() override {
+		database->Disconnect();
+		delete database;
+	}
+};
+
+TEST_F(DatabaseTest, Login) {
+	database->Login("test_id", "test_password");
+	
+	ASSERT_TRUE(database->IsLogin()) << "로그인 하였을 때";
+}
+
+TEST_F(DatabaseTest, Logout) {
+	database->Login("test_id", "test_password");
+	database->Logout();
+	
+	ASSERT_FALSE(database->IsLogin()) << "로그아웃 하였을 때";
+}
+
