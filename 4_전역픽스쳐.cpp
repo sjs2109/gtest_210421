d716fbdf3b951e8700929b3e164fede1ccb36aa2
Chansik Yun <chansigi@gmail.com>
2021-04-21 06:28:23 +0000

    .

--- 4_전역픽스쳐.cpp
+++ 4_전역픽스쳐.cpp
@@ -20,14 +20,36 @@ public:
 	}
 };
 
+class TestEnvironment2 : public testing::Environment {
+public:
+	TestEnvironment2() { printf("TestEnvironment2()\n"); }
+	~TestEnvironment2() { printf("~TestEnvironment2()\n"); }
+
+	void SetUp() override {
+		printf("Global SetUp2()\n");
+	}
+
+	void TearDown() override {
+		printf("Global TearDown2()\n");
+	}
+};
+
 TEST(SampleTest, foo) {}
 TEST(MyTest, goo) {}
 
 // 등록하는 방법 2가지
 //  1) main함수 직접 제공하지 않을 때 - 전역 변수
-testing::Environment* my_env = testing::AddGlobalTestEnvironment(new TestEnvironment);
+// testing::Environment* my_env = testing::AddGlobalTestEnvironment(new TestEnvironment);
 
-//  2) main함수 직접 제공하고 있을 때
+//  2) main함수 직접 제공하고 있을 때 - 권장!
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+
+	testing::AddGlobalTestEnvironment(new TestEnvironment);
+	testing::AddGlobalTestEnvironment(new TestEnvironment2);
+
+	return RUN_ALL_TESTS();
+}
 
 
 
