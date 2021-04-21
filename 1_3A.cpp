e80703a28c6060345e93a9e73dd877f0a14c1997
Chansik Yun <chansigi@gmail.com>
2021-04-21 02:35:49 +0000

    .

---
+++ 1_3A.cpp
@@ -0,0 +1,50 @@
+
+// 1_3A.cpp
+#include <gtest/gtest.h>
+
+// 단위 테스트를 만드는 방법
+//  - Google Test 1.10 이전
+//  - xUnit Test Framework의 표준 용어와 차이가 있었습니다.
+//
+//  TestCase
+//    - Test
+//    - Test
+//    - Test
+//  TestCase
+//    - Test
+//    - Test
+//------------------------------------
+//  - Google Test 1.10 이후
+//  - xUnit Test Framework 표준 용어
+//
+//  TestSuite
+//   - TestCase
+//   - TestCase
+//   - TestCase
+//  TestSuite
+//   - TestCase
+//   - TestCase
+
+// * 구글 테스트에서 TestCase 를 추가하는 방법.
+// : TEST(TestSuite 이름, TestCase 이름)
+
+TEST(SampleTest, Sample1) {
+}
+
+TEST(SampleTest, Sample2) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
