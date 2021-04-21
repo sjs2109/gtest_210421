232b3df32163953cd3a463cdbe238e3c7f2603d0
Chansik Yun <chansigi@gmail.com>
2021-04-21 05:34:59 +0000

    .

---
+++ 3_스위트픽스쳐.cpp
@@ -0,0 +1,82 @@
+// 3_스위트픽스쳐.cpp
+
+#include <string>
+
+class Database {
+public:
+	void Connect() {}
+	void Disconnect() {}
+
+	void Login(const std::string& id, const std::string& password) {}
+	void Logout() {}
+
+	bool IsLogin() { return true; }
+};
+
+//--------------------
+
+class DatabaseTest : public testing::Test {
+protected:
+	Database* database;
+
+	DatabaseTest() : database(nullptr) {}
+
+	void SetUp() override {
+		database = new Database;
+		database->Connect();
+	}
+
+	void TearDown() override {
+		database->Disconnect();
+		delete database;
+	}
+};
+
+TEST_F(DatabaseTest, Login) {
+	database->Login("test_id", "test_password");
+	
+	ASSERT_TRUE(database->IsLogin()) << "로그인 하였을 때";
+}
+
+TEST_F(DatabaseTest, Logout) {
+	database->Login("test_id", "test_password");
+	database->Logout();
+	
+	ASSERT_FALSE(database->IsLogin()) << "로그아웃 하였을 때";
+}
+
+
+#if 0
+TEST(DatabaseTest, Login) {
+	Database* database = new Database;
+	database->Connect();
+
+	database->Login("test_id", "test_password");
+	
+	ASSERT_TRUE(database->IsLogin()) << "로그인 하였을 때";
+	
+	database->Disconnect();
+	delete database;
+}
+
+TEST(DatabaseTest, Logout) {
+	Database* database = new Database;
+	database->Connect();
+
+	database->Login("test_id", "test_password");
+	database->Logout();
+	
+	ASSERT_FALSE(database->IsLogin()) << "로그아웃 하였을 때";
+	
+	database->Disconnect();
+	delete database;
+}
+#endif
+
+
+
+
+
+
+
+
