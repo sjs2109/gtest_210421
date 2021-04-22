cb8213e2064dfb2b8b33917ef38035a807f351db
Chansik Yun <chansigi@gmail.com>
2021-04-22 01:03:31 +0000

    .

--- 7_FriendTest.cpp
+++ 7_FriendTest.cpp
@@ -10,22 +10,27 @@ public:
 		age += 1;
 	}
 
-protected: // ??
-private:   // ??
+protected: 
 	int GetAge() const {
 		return age;
 	}
 };
 
 //------------
-
 #include <gtest/gtest.h>
+// 접근하는 속성이 protected 인 경우
+//  => 테스트 전용 하위 클래스
+
+class TestUser : public User {
+public:
+	using User::GetAge;  // protected ->public
+};
 
 TEST(UserTest, Do) {
-	User user;
+	// User user;
+	TestUser user;
 
 	user.Do();
 
 	EXPECT_EQ(43, user.GetAge());
 }
-
