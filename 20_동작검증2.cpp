4db6aebcb1d4c965360b69728ab5929a1a42772a
Chansik Yun <chansigi@gmail.com>
2021-04-23 06:06:44 +0000

    .

--- 20_동작검증2.cpp
+++ 20_동작검증2.cpp
@@ -1,20 +1,11 @@
+#include <stdio.h>
+
 struct Calc {
 	virtual ~Calc() {}
 
 	virtual int Add(int a, int b) = 0;
 	virtual int Sub(int a, int b) = 0;
 };
-
-void UseCalc(Calc* p) {
-	printf("Add: %d\n", p->Add(10, 20));
-	printf("Sub: %d\n", p->Sub(30, 20));
-
-#if 0
-	if (p->Add(10, 20) == 30) {
-		printf("Sub: %d\n", p->Sub(30, 20));
-	}
-#endif
-}
 
 #include <gmock/gmock.h>
 
@@ -27,14 +18,34 @@ public:
 // ON_CALL / EXPECT_CALL 을 같이 사용하고 있다면,
 //  => ON_CALL을 사용할 필요가 없습니다.
 //  => EXPECT_CALL을 통해 결과를 제어하는 것이 가능합니다.
+//  1. WillOnce
+//  2.
 
+// 횟수
+//  1. WillOnce + WillOnce + WillOnce  => Times(3)
+
+void UseCalc(Calc* p) {
+	printf("Add: %d\n", p->Add(10, 20));
+	printf("Add: %d\n", p->Add(10, 20));
+	printf("Add: %d\n", p->Add(10, 20));
+	printf("Add: %d\n", p->Add(10, 20));
+	printf("Add: %d\n", p->Add(10, 20));
+	// printf("Sub: %d\n", p->Sub(30, 20));
+}
+
+using testing::Return;
 TEST(CalcTest, UseCalc) {
 	MockCalc mock;
-	ON_CALL(mock, Add).WillByDefault(Return(30));
-	ON_CALL(mock, Sub).WillByDefault(Return(10));
 	
-	EXPECT_CALL(mock, Add);
-	EXPECT_CALL(mock, Sub(30, 20));
+	EXPECT_CALL(mock, Add)
+		.WillOnce(Return(30))
+		.WillOnce(Return(20))
+		.WillOnce(Return(10))
+		.WillOnce(Return(20))
+		.WillOnce(Return(30));
+
+	// EXPECT_CALL(mock, Sub(30, 20))
+	//	.WillOnce(Return(10));
 	
 	UseCalc(&mock);
 }
